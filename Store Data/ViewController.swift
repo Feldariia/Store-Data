a0bf806b6da63b0dfb7af88a472f825ea116d1da
Shannon <s.mong@setonhill.edu>
2020-04-23 11:25:11 -0400

    Initial Commit

---
+++ Store Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Store Data
+//
+//  Created by Shannon Mong on 4/23/20.
+//  Copyright © 2020 Shannon Mong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
